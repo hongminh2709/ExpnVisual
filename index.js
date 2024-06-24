28b108f296bb5f0507eccd403043337fc57eea14
minhlh <minhlh@mor.com.vn>
2024-06-24 16:08:04 +0700

    update layout pc 24062024

--- index.js
+++ index.js
@@ -70,3 +70,21 @@ if (width > 1024) {
     });
   });
 }
+document.addEventListener("DOMContentLoaded", function () {
+  const links = document.querySelectorAll(".scroll-link");
+
+  links.forEach((link) => {
+    link.addEventListener("click", function (e) {
+      e.preventDefault();
+      const targetId = this.getAttribute("href").substring(1);
+      const targetElement = document.getElementById(targetId);
+
+      window.scrollTo({
+        top:
+          targetElement.offsetTop -
+          document.querySelector("header").offsetHeight,
+        behavior: "smooth",
+      });
+    });
+  });
+});
