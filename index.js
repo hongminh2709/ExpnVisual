8bd96f8a25f212eb49c0c2612f2ad90c87603fc4
minhlh <minhlh@mor.com.vn>
2024-06-24 12:18:11 +0700

    implement update template 24062024

--- index.js
+++ index.js
@@ -29,4 +29,44 @@ $(document).ready(function () {
       },
     ],
   });
+  $(".btn-mobile").click(function () {
+    $(".btn-mobile").toggleClass("show");
+    $("header").toggleClass("show");
+    $(".bgr-mobile").toggleClass("show");
+  });
+  $(".bgr-mobile").click(function () {
+    $(".btn-mobile").removeClass("show");
+    $("header").removeClass("show");
+    $(".bgr-mobile").removeClass("show");
+  });
 });
+let width = window.innerWidth > 0 ? window.innerWidth : screen.width;
+if (width > 1024) {
+  const sliders = document.querySelectorAll(".portfolio__list");
+  let isDown = false;
+  let startX;
+  let scrollLeft;
+  sliders.forEach((slider) => {
+    slider.addEventListener("mousedown", (e) => {
+      isDown = true;
+      slider.classList.add("active");
+      startX = e.pageX - slider.offsetLeft;
+      scrollLeft = slider.scrollLeft;
+    });
+    slider.addEventListener("mouseleave", () => {
+      isDown = false;
+      slider.classList.remove("active");
+    });
+    slider.addEventListener("mouseup", () => {
+      isDown = false;
+      slider.classList.remove("active");
+    });
+    slider.addEventListener("mousemove", (e) => {
+      if (!isDown) return;
+      e.preventDefault();
+      const x = e.pageX - slider.offsetLeft;
+      const walk = (x - startX) * 3;
+      slider.scrollLeft = scrollLeft - walk;
+    });
+  });
+}
